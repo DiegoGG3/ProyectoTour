e591e180a5c0e56e4b9876bc2361d562376bee85
diego <dgargay987@g.educaand.es>
2024-02-07 13:42:46 +0100

    commit Footer

--- assets/js/landin.js
+++ assets/js/landin.js
@@ -191,10 +191,5 @@ $(function() {
             });
         });
     
-        // Activate SimpleLightbox plugin for portfolio items
-        new SimpleLightbox({
-            elements: '#portfolio a.portfolio-box'
-        });
-    
     
 });
