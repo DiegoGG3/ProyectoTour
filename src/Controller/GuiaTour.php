694b58bfdc63d47504f47a1b657ad08866cec818
diego <dgargay987@g.educaand.es>
2024-02-26 23:46:41 +0100

    commit RutaGuia

--- src/Controller/GuiaTour.php
+++ src/Controller/GuiaTour.php
@@ -6,17 +6,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Tour; 
+use App\Entity\Tour;
+use App\Entity\User;
 
-class AdministrarTour extends AbstractController
+class GuiaTour extends AbstractController
 {
-    #[Route('/administrar_ruta/{idGuia}', name: 'administrar_ruta')]
+    #[Route('/guia_ruta/{idGuia}', name: 'guia_ruta')]
     public function index(EntityManagerInterface $entityManager, $idGuia): Response
     {
-        $tours = $entityManager->getRepository(Tour::class)->findBy(['guia_id' => $idGuia]);
+        $guia = $entityManager->getRepository(User::class)->findBy(['id' => $idGuia]);
+        $tours = $entityManager->getRepository(Tour::class)->findBy(['guia' => $guia]);
 
-        return $this->render('crear_ruta/index.html.twig', [
-            'controller_name' => 'administrar_tours',
+        return $this->render('guia_tours.html.twig', [
             'tours' => $tours
         ]);
     }
