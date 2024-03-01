7e0e060c026e7387931229de7dd137345db4f3ce
diego <dgargay987@g.educaand.es>
2024-03-01 15:45:04 +0100

    commit Login

--- src/Controller/GuiaTour.php
+++ src/Controller/GuiaTour.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Controller;
 
-
+use App\Entity\Ruta;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,8 +17,9 @@ class GuiaTour extends AbstractController
         $guia = $entityManager->getRepository(User::class)->findBy(['id' => $idGuia]);
         $tours = $entityManager->getRepository(Tour::class)->findBy(['guia' => $guia]);
 
+
         return $this->render('guia_tours.html.twig', [
-            'tours' => $tours
+            'tours' => $tours,
         ]);
     }
 }
